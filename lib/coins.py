bee0a63e170bcdb73ad597f097753049f2ab1036
TheLazieR Yip <thelazier@gmail.com>
2017-02-02 09:19:47 +0700

    Update PEER_DEFAULT_PORTS for DASH Testnet

--- lib/coins.py
+++ lib/coins.py
@@ -459,8 +459,9 @@ class DashTestnet(Dash):
     TX_PER_BLOCK = 1
     RPC_PORT = 19998
     IRC_PREFIX = "d_"
+    PEER_DEFAULT_PORTS = {'t':'51001', 's':'51002'}
     PEERS = [
-        'electrum.dash.siampm.com s51002 t51001',
+        'electrum.dash.siampm.com s t',
     ]
 
 
