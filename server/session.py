9e34bf858320d9b98aff687509451df24450251c
LaoDC <github@laodc.com>
2017-05-03 17:43:47 +0700

    reverted and made the version split isolated within the banner logic only as not to affect other parts of the code.

--- server/session.py
+++ server/session.py
@@ -288,10 +288,11 @@ class ElectrumX(SessionBase):
         minor, revision = divmod(minor, 10000)
         revision //= 100
         daemon_version = '{:d}.{:d}.{:d}'.format(major, minor, revision)
+        server_version = version.VERSION.split()[-1]
         for pair in [
-                ('$VERSION', version.SUB_VERSION), # legacy
-                ('$SERVER_VERSION', version.VERSION),
-                ('$SERVER_SUBVERSION', version.SUB_VERSION),
+                ('$VERSION', version.VERSION), # legacy
+                ('$SERVER_VERSION', server_version),
+                ('$SERVER_SUBVERSION', version.VERSION),
                 ('$DAEMON_VERSION', daemon_version),
                 ('$DAEMON_SUBVERSION', network_info['subversion']),
                 ('$DONATION_ADDRESS', self.env.donation_address),
--- server/version.py
+++ server/version.py
@@ -1,6 +1,5 @@
 # Server name and protocol versions
 
-VERSION = '1.0.10'
-SUB_VERSION = 'ElectrumX {}'.format(VERSION)
+VERSION = 'ElectrumX 1.0.10'
 PROTOCOL_MIN = '1.0'
 PROTOCOL_MAX = '1.0'
