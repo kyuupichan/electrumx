c7a2ebd1a02e2aaf967354f456024a7e2001c6a8
Daniel Shakhmundes <DaShak@users.noreply.github.com>
2018-06-14 10:57:22 -0400

    Hardcoded username directory replaced with ~
    This change will cd to the current user's directory, rather than /home/username

--- contrib/python3.6/python-3.6.sh
+++ contrib/python3.6/python-3.6.sh
@@ -6,7 +6,7 @@
 sudo add-apt-repository ppa:jonathonf/python-3.6
 sudo apt-get update && sudo apt-get install python3.6 python3.6-dev
 
-cd /home/username
+cd ~
 git clone https://github.com/kyuupichan/electrumx.git
 cd electrumx
 sudo python3.6 setup.py install
