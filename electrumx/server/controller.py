89cfb03e38aecdfcbc53a7ee7b589d98da8ace64
Lex Berezhny <lex@damoti.com>
2018-07-10 16:04:09 -0400

    renamed tcp_server_started to server_listening and added explanation of Event

--- electrumx/server/controller.py
+++ electrumx/server/controller.py
@@ -102,7 +102,8 @@ class Controller(ServerBase):
         self.mempool = MemPool(self.bp, self)
         self.peer_mgr = PeerManager(env, self)
 
-        self.tcp_server_started = asyncio.Event()
+        # Event triggered when electrumx is listening for incoming requests.
+        self.server_listening = asyncio.Event()
 
     @classmethod
     def short_version(cls):
@@ -294,7 +295,7 @@ class Controller(ServerBase):
             sslc = ssl.SSLContext(ssl.PROTOCOL_TLS)
             sslc.load_cert_chain(env.ssl_certfile, keyfile=env.ssl_keyfile)
             await self.start_server('SSL', host, env.ssl_port, ssl=sslc)
-        self.tcp_server_started.set()
+        self.server_listening.set()
 
     def notify_sessions(self, touched):
         '''Notify sessions about height changes and touched addresses.'''
