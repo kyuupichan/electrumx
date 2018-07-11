63c513337cf9cd9695a87d4403e40345fa246e59
Lex Berezhny <lex@damoti.com>
2018-07-10 21:02:31 -0400

    added compact log formatter that strips module name

--- electrumx_server.py
+++ electrumx_server.py
@@ -9,16 +9,29 @@
 
 '''Script to kick off the server.'''
 
+import sys
 import logging
 import traceback
 
 from electrumx import Controller, Env
 
 
+class CompactFormatter(logging.Formatter):
+    def format(self, record):
+        record.name = record.name.split('.')[-1]
+        return super().format(record)
+
+
 def main():
     '''Set up logging and run the server.'''
+
     log_fmt = Env.default('LOG_FORMAT', '%(levelname)s:%(name)s:%(message)s')
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
+    log = logging.getLogger('electrumx')
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setFormatter(CompactFormatter(log_fmt))
+    log.addHandler(handler)
+    log.setLevel(logging.INFO)
+
     logging.info('ElectrumX server starting')
     try:
         controller = Controller(Env())
