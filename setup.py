5a42f79bb863c9cb1a73ed5a69f3c2f21d7d0162
Neil Booth <kyuupichan@gmail.com>
2018-07-25 11:07:14 +0800

    Require an exact aiorpcX version
    The interface is changing; best to target a single codebase

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     # "tribus_hash" package is required to sync Denarius network.
     # "blake256" package is required to sync Decred network.
     # "xevan_hash" package is required to sync Xuez network.
-    install_requires=['aiorpcX >= 0.5.6', 'attrs>=15',
+    install_requires=['aiorpcX == 0.5.6', 'attrs>=15',
                       'plyvel', 'pylru', 'aiohttp >= 2'],
     packages=setuptools.find_packages(include=('electrumx*',)),
     description='ElectrumX Server',
