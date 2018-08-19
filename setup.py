f9580d947c660d2fecc19b67dc41033c7aedcd6a
脇山P <wakiyamap@gmail.com>
2018-08-19 21:28:32 +0900

    update aiorpcX

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     # "blake256" package is required to sync Decred network.
     # "xevan_hash" package is required to sync Xuez network.
     # "groestlcoin_hash" package is required to sync Groestlcoin network.
-    install_requires=['aiorpcX>=0.7.1,<0.8', 'attrs',
+    install_requires=['aiorpcX>=0.7.3,<0.8', 'attrs',
                       'plyvel', 'pylru', 'aiohttp >= 2'],
     packages=setuptools.find_packages(include=('electrumx*',)),
     description='ElectrumX Server',
