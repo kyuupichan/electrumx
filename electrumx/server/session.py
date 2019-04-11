90e2cf6efd51b38d12f220c8013b2109bfe62379
Neil Booth <kyuupichan@gmail.com>
2019-04-11 10:12:29 +0100

    Process sessions most-expensive first
    So if they are cut off they no longer contribute to the group cost
    of other sessions

--- electrumx/server/session.py
+++ electrumx/server/session.py
@@ -234,7 +234,7 @@ class SessionManager(object):
         session_class = self.env.coin.SESSIONCLS
         while True:
             await sleep(100)
-            for session in self.sessions:
+            for session in sorted(self.sessions, key=lambda s: s.cost, reverse=True):
                 # Subs have an on-going cost so decay more slowly with more subs
                 session.cost_decay_per_sec = (
                     session_class.cost_hard_limit / (10000 + 5 * session.sub_count()))
