22cd94d05ec84d86a35e5aec1215994015d750c5
sherlockcoin <sherlockcoin@gmail.com>
2019-07-14 08:20:46 +0530

    [UNO]Added Unobtanium Support (UNO) Accidental Versioning Change
    [UNO]Added Unobtanium Support (UNO) Accidental Versioning Change

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
-VERSION="ElectrumX 1.12.1"
+VERSION="ElectrumX 1.12.0"
 
 # -- Project information -----------------------------------------------------
 
