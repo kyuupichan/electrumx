7d2aa76cba8123111130d1a03361a1fcbbd2178d
jakesum <57389504+jakesum@users.noreply.github.com>
2020-01-23 01:14:22 -0700

    Adjusted indentations

--- electrumx/lib/coins.py
+++ electrumx/lib/coins.py
@@ -876,24 +876,24 @@ class BitcoinNolnet(BitcoinCash):
 
 	
 class Sumcoin(Coin):
-	NAME = "Sumcoin"
-	SHORTNAME = "SUM"
-	NET = "mainnet"
-	XPUB_VERBYTES = bytes.fromhex("0488b41c")
-	XPRV_VERBYTES = bytes.fromhex("0488abe6")
-	P2PKH_VERBYTE = bytes.fromhex("3F")
-	P2SH_VERBYTES = [bytes.fromhex("C8"), bytes.fromhex("05")]
-	WIF_BYTE = bytes.fromhex("bf")
-	GENESIS_HASH = ('37d4696c5072cd012f3b7c651e5ce56a'
-					'1383577e4edacc2d289ec9b25eebfd5e')
-	DESERIALIZER = lib_tx.DeserializerSegWit
-	TX_COUNT = 976394
-	TX_COUNT_HEIGHT = 659520
-	TX_PER_BLOCK = 2
-	RPC_PORT = 3332
-	REORG_LIMIT = 800
-	PEER_DEFAULT_PORTS = {'t': '53332', 's': '53333'}
-	PEERS = []
+    NAME = "Sumcoin"
+    SHORTNAME = "SUM"
+    NET = "mainnet"
+    XPUB_VERBYTES = bytes.fromhex("0488b41c")
+    XPRV_VERBYTES = bytes.fromhex("0488abe6")
+    P2PKH_VERBYTE = bytes.fromhex("3F")
+    P2SH_VERBYTES = [bytes.fromhex("C8"), bytes.fromhex("05")]
+    WIF_BYTE = bytes.fromhex("bf")
+    GENESIS_HASH = ('37d4696c5072cd012f3b7c651e5ce56a'
+		    '1383577e4edacc2d289ec9b25eebfd5e')
+    DESERIALIZER = lib_tx.DeserializerSegWit
+    TX_COUNT = 976394
+    TX_COUNT_HEIGHT = 659520
+    TX_PER_BLOCK = 2
+    RPC_PORT = 3332
+    REORG_LIMIT = 800
+    PEER_DEFAULT_PORTS = {'t': '53332', 's': '53333'}
+    PEERS = []
 
 	
 class Litecoin(Coin):
