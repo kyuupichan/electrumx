1449cfbdf15cbf19598d63c85e66f295a3aed94e
Sumcoin <37975862+sumcoinlabs@users.noreply.github.com>
2020-02-01 14:38:16 -0700

    Sumcoin support for electrumx (#945)
    * SUM added to coins.py

--- electrumx/lib/coins.py
+++ electrumx/lib/coins.py
@@ -875,6 +875,28 @@ class BitcoinNolnet(BitcoinCash):
     PEER_DEFAULT_PORTS = {'t': '52001', 's': '52002'}
 
 
+# Source: https://github.com/sumcoinlabs/sumcoin
+class Sumcoin(Coin):
+    NAME = "Sumcoin"
+    SHORTNAME = "SUM"
+    NET = "mainnet"
+    XPUB_VERBYTES = bytes.fromhex("0488b41c")
+    XPRV_VERBYTES = bytes.fromhex("0488abe6")
+    P2PKH_VERBYTE = bytes.fromhex("3f")
+    P2SH_VERBYTES = [bytes.fromhex("c8"), bytes.fromhex("05")]
+    WIF_BYTE = bytes.fromhex("bf")
+    GENESIS_HASH = ('37d4696c5072cd012f3b7c651e5ce56a'
+                    '1383577e4edacc2d289ec9b25eebfd5e')
+    DESERIALIZER = lib_tx.DeserializerSegWit
+    TX_COUNT = 976394
+    TX_COUNT_HEIGHT = 659520
+    TX_PER_BLOCK = 2
+    REORG_LIMIT = 800
+    RPC_PORT = 3332
+    PEER_DEFAULT_PORTS = {'t': '53332', 's': '53333'}
+    PEERS = []
+
+
 class Litecoin(Coin):
     NAME = "Litecoin"
     SHORTNAME = "LTC"
